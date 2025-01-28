42392847246d9c0fdce5c65b709452790e7dabe2
Mulwor <adigezalli@yandex.ru>
2025-01-28 11:07:33 +0400

    init: project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Router } from "react-router";
+import "./App.css";
+import { Navbar } from "./components/Menu/Menu";
+
+
+function App() {
+  return (
+    <Navbar />
+  )
+}
+
+export default App;
