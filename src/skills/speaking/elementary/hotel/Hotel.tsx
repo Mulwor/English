ed60077d48d45e4d6822b69ee4259ca6aa6a9083
Mulwor <adigezalli@yandex.ru>
2026-02-17 23:05:18 +0100

    update

--- src/skills/speaking/elementary/hotel/Hotel.tsx
+++ src/skills/speaking/elementary/hotel/Hotel.tsx
@@ -1,6 +1,6 @@
 import { Collapse, Divider } from 'antd';
 import { CollapseWithAccordion } from '../../../../components/Collapse';
-import { allHotel, hotel_01, hotel_02, hotel_03, hotel_04, hotel_05, hotel_06, hotel_07, hotel_08, hotel_09, hotel_10 } from './hotel';
+import { allHotel, hotel_01, hotel_03, hotel_05, hotel_07, hotel_09 } from './hotel';
 import { columns } from '../../../../data/sample';
 import { usePaginatedData } from '../../../../hooks/usePaginatedData';
 import { BasicTable, Pagination, Video } from '../../../../components';
@@ -10,15 +10,10 @@ const { Panel } = Collapse;
 export const Hotel = () => {
   const { data, currentPage, handlePageChange } = usePaginatedData(hotel_01, [
     hotel_01,
-    hotel_02,
     hotel_03,
-    hotel_04,
     hotel_05,
-    hotel_06,
     hotel_07,
-    hotel_08,
     hotel_09,
-    hotel_10,
   ]);
 
   return (
@@ -48,7 +43,7 @@ export const Hotel = () => {
       <BasicTable data={data} />
       <Pagination
         currentPage={currentPage}
-        totalPages={10}
+        totalPages={5}
         onPageChange={handlePageChange}
       />
     </div>
